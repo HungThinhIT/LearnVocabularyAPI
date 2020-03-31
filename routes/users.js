3b21082141038ff0bc2f4a6e57009daddbdab22d
HungThinh0710 <nhtnokia@gmail.com>
2020-04-01 01:01:27 +0700

    Update ref for [user] docs API

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const auth = require("../middleware/auth")
 const userValidate = require("../validates/validate.user") 
 
 router.post("/register", userController.create)
-router.post("/login", userValidate.login, userController.login)
+router.post("/", userValidate.login, userController.login)
 router.get("/", auth ,userController.getUser)
 router.patch("/", auth, userController.update)
 router.post("/password", auth, userController.changePassword)
@@ -17,22 +17,103 @@ router.post("/logout", auth, userController.logout)
 //DOCUMENTATION BELOW
 /**
  * @swagger
- * tags:
+ *  tags:
  *   name: Users
  *   description: User management
  */
 
 /**
  * @swagger
+ * /user/register:
+ *  post:
+ *      summary: [22:15_29032020-BUG]
+ *      tags: [Users]
+ *      description: Register.
+ *      parameters:
+ *          - in: body
+ *            name: name
+ *            required: true
+ *            schema:
+ *              type: string
+ *            description: Your fullname.
+ *          - in: body
+ *            name: email
+ *            required: true
+ *            schema:
+ *              type: string
+ *            description: Email of account.
+ *          - in: body
+ *            name: password
+ *            required: true
+ *            schema:
+ *              type: string
+ *            description: Password of account.
+ *      responses: 
+ *          '':
+ *              description: .
+ *  
  * /user:
  *  get:
- *    tags: [Users]
- *    description: Use to get information of current user.
- *    security:
- *     - Bearer: []
- *    responses: 
- *      '200':
- *        description: Hello! This's response from UserController
+ *      tags: [Users]
+ *      description: Use to get information of current user.
+ *      security:
+ *      - Bearer: []
+ *      responses: 
+ *          '':
+ *              description: .
+ *  post:
+ *      tags: [Users]
+ *      description: Login.
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email: 
+ *                              $ref: '#/components/schemas/User/properties/email'
+ *                          password: 
+ *                              $ref: '#/components/schemas/User/properties/password'
+ *                  required:
+ *                      - email
+ *                      - password
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email: 
+ *                              $ref: '#/components/schemas/User/properties/email'
+ *                          password: 
+ *                              $ref: '#/components/schemas/User/properties/password'
+ *                  required:
+ *                      - email
+ *                      - password
+ *      responses: 
+ *          '':
+ *              description: .
+ *  
+ * components:
+ *      schemas:
+ *          User:
+ *              type: object
+ *              properties:
+ *                   email:
+ *                       type: string
+ *                       description: Email of account
+ *                       example: example@hungthinhit.com
+ *                   password:
+ *                       type: string
+ *                   isVerifyEmail:
+ *                       type: integer
+ *                   userType:
+ *                       type: string(ref to UserTypeTable)
+ *                   createdAt:
+ *                       type: timestamps
+ *                   updatedAt:
+ *                       type: timestamps
+ *              xml:
+ *                  name: User
  */
 
 module.exports = router;
